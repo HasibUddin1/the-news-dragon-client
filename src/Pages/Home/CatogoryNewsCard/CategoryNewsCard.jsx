bfb8b72482196a749a09de4a5864c4c38ee0b7fa
IconicLuck <uhasib959@gmail.com>
2023-04-23 21:14:46 +0600

    added header section and footer section informations of a news card

--- src/Pages/Home/CatogoryNewsCard/CategoryNewsCard.jsx
+++ src/Pages/Home/CatogoryNewsCard/CategoryNewsCard.jsx
@@ -1,15 +1,27 @@
+import moment from 'moment';
 import React from 'react';
 import { Card, Image } from 'react-bootstrap';
+import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from 'react-icons/fa';
+import Rating from 'react-rating';
 import { Link } from 'react-router-dom';
 
 const CategoryNewsCard = ({ news }) => {
 
-    const { _id,title, details, image_url, author } = news
+    const { _id, title, details, image_url, author, total_view, rating } = news
 
     return (
         <Card className="mb-4">
-            <Card.Header>
-                <Image src={author.img} roundedCircle />
+            <Card.Header className='d-flex align-items-center'>
+                <div className='d-flex gap-2 align-items-center flex-grow-1'>
+                    <Image style={{ height: '40px', width: '40px' }} src={author.img} roundedCircle />
+                    <div>
+                        <p className='mb-0'>{author?.name}</p>
+                        <p className='mb-0'><small>{moment(author?.published_date).format('YYYY-MM-DD')}</small></p>
+                    </div>
+                </div>
+                <div>
+                    <FaRegBookmark></FaRegBookmark> <FaShareAlt></FaShareAlt>
+                </div>
             </Card.Header>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
@@ -17,12 +29,27 @@ const CategoryNewsCard = ({ news }) => {
                 <Card.Text>
                     {
                         details.length < 250 ?
-                        <>{details}</> :
-                        <>{details.slice(0, 250)}....... <Link to={`/news/${_id}`}>Read More</Link> </>
+                            <>{details}</> :
+                            <>{details.slice(0, 250)}....... <Link to={`/news/${_id}`}>Read More</Link> </>
                     }
                 </Card.Text>
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            <Card.Footer className="text-muted d-flex">
+                <div className='d-flex align-items-center gap-2 flex-grow-1'>
+                    <Rating
+                        placeholderRating={rating?.number}
+                        emptySymbol={<FaRegStar></FaRegStar>}
+                        placeholderSymbol={<FaStar></FaStar>}
+                        fullSymbol={<FaStar></FaStar>}
+                        readonly
+                    ></Rating>
+                    <p className='mb-0'>{rating?.number}</p>
+                </div>
+                <div className='d-flex gap-2 align-items-center'>
+                    <FaEye></FaEye>
+                    <p className='mb-0'>{total_view}</p>
+                </div>
+            </Card.Footer>
         </Card>
     );
 };
