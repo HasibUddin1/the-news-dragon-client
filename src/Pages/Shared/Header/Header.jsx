fc96f9ee2e09c0b05f321238b9dfb22dc6a81408
IconicLuck <uhasib959@gmail.com>
2023-04-24 21:48:14 +0600

    added new route for login and register purpose

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,15 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container} from 'react-bootstrap';
 import Marquee from 'react-fast-marquee';
-import { Link } from 'react-router-dom';
-import { FaUserCircle } from 'react-icons/fa';
-import { AuthContext } from '../../../providers/AuthProviders';
 
 const Header = () => {
-
-    const { user } = useContext(AuthContext)
 
     return (
         <Container>
@@ -24,32 +19,6 @@ const Header = () => {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam in ea et dolorum saepe sint voluptatibus officiis perferendis soluta doloribus commodi, placeat sed. Voluptates fuga quis quam blanditiis pariatur eaque.
                 </Marquee>
             </div>
-            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-                <Container>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#features">
-                                <Link to='/'>Home</Link>
-                            </Nav.Link>
-                            <Nav.Link href="#pricing">About</Nav.Link>
-                            <Nav.Link href="#pricing">Career</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link href="#deets"><FaUserCircle style={{ fontSize: '2rem' }}></FaUserCircle></Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                {
-                                    user ?
-                                        <Button variant="secondary">Logout</Button> :
-                                        <Link to='/login'>
-                                            <Button variant="secondary">Login</Button>
-                                        </Link>
-                                }
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
         </Container>
     );
 };
